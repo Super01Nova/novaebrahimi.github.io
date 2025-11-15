701f40b2246e063cad12b6197ac02e3309f6d7d4
Super01Nova <119613304+Super01Nova@users.noreply.github.com>
2025-11-15 02:41:03 +0000

    Deploying to gh-pages from @ Super01Nova/novaebrahimi.github.io@baead4bc46707b63a7cf2dcfa743d10b72561fe1 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@{{version}}/dist/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@{{version}}/dist/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@3.6.0/dist/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@3.6.0/dist/photoswipe.esm.min.js";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
